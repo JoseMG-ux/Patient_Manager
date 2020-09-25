d6e1dc1afc77d4b903e8b29e69fb1662d3c50f8f
JoseMG-ux <joseutp14@gmail.com>
2020-09-25 02:07:04 -0600

    Add data in DB with postmand and model

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -1,7 +1,15 @@
-
+const Paciente = require('../models/Paciente')
 //When create new client
-exports.nuevoCliente = ( req, res, next ) => {
-    -//TODO: Insert in BD
-    console.log(req.body)
-    res.json({ mensaje : 'El Cliente se agrego correctamente' });
+exports.nuevoCliente = async ( req, res, next ) => {
+    //Create client object whit rq.body data
+    const paciente = new Paciente(req.body)
+
+    try {
+        await paciente.save();
+        res.json({ mensaje : 'El Cliente se agrego correctamente' });
+    } catch (error) {
+        console.log(error);
+        next();
+    };
+    
 }
