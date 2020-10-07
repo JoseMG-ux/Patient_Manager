2815ed4c545a178ae046310860d0be678c836870
JoseMG-ux <joseutp14@gmail.com>
2020-10-07 01:45:41 -0600

    Add endpoint for delete

--- API/routes/index.js
+++ API/routes/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const pacienteController = require('../controllers/pacienteControllers.js');
+const pacienteController = require('../controllers/pacienteControllers');
 
 module.exports = function(){
     //Add new client POST
     router.post('/pacientes',
         pacienteController.nuevoCliente
-    )
+    );
+
+    //get all records from the DB
+    router.get('/pacientes',
+        pacienteController.obtenerPacientes
+    );
+
+    //get specific patient
+    router.get('/pacientes/:id',
+        pacienteController.pacienteEspecifico
+    );
+
+    //update a record with ID
+    router.put('/pacientes/:id', 
+        pacienteController.actualizarRegistro
+    );
+
+    // //Delete patient for id
+    router.delete('/pacientes/:id', 
+        pacienteController.eliminarRegistro
+    );
     return router;
 }
